a86766626eb8abfc064d5add7e011dd8ecdd4803
Ajaykumarunt <Ajaykumar.unt@gmail.com>
2023-12-04 20:37:07 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example JavaScript to handle button clicks
+document.querySelectorAll('.product button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('More information coming soon!');
+    });
+});
